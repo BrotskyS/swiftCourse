89d02c6d4ee636d16a8ff6829eefe31b93ba05b3
Brotsky <sergiypik.work@gmail.com>
2021-11-13 15:20:24 +0200

    Initial Commit

---
+++ courseIOS15/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  courseIOS15
+//
+//  Created by Brotsky on 11/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ courseIOS15/courseIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  courseIOS15App.swift
+//  courseIOS15
+//
+//  Created by Brotsky on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct courseIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
